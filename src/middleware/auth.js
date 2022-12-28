5491fe9f9eb225a61ab6de8b91c99b05c016d3c9
Alfiya <alfiya96960@gmail.com>
2022-12-28 20:28:09 +0530

    authenticate and authorization added

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,42 @@
-const authenticate = function(req, req, next) {
+
+const jwt =require("jsonwebtoken")
+
+
+exports.authenticate = function(req, req, next) {
     //check the token in request header
     //validate this token
+    let token=req.headers["x-auth-token"]
+    if(!token){
+        return res.json({status:false,msg:"token is mandatory"})
+
+    }
+    let decodedtoken=jwt.verify(token,'functionup-thorium')
+    if(!decodedtoken){
+        return res.json({status:false,msg:"token is invalid"})
+    }
 
     next()
 }
 
 
-const authorise = function(req, res, next) {
+exports.authorise = function(req, res, next) {
     // comapre the logged in user's id and the id in request
+    let userId=req.params.userId
+    let token=req.headers["x-auth-token"]
+    if(!token){
+        return res.json({status:false,msg:"token is mandatory"})
+
+    }
+    let decodedtoken=jwt.verify(token,'functionup-thorium')
+    if(!decodedtoken){
+        return res.json({status:false,msg:"token is invalid"})
+    }
+
+    if(decodedtoken.userId!=userId){
+        return res.json({status:false,msg:"unauthorised user"})
+    }
+
     next()
 }
+
+
